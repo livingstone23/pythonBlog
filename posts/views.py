8296b49f207894cd8afde4e83fa2232fcec8da13
livingstone23 <livingstone23@gmail.com>
2022-11-09 17:17:03 +0100

    Complete section Django

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.generic.base import View
 
 
 class HelloWorld(View):
     def get(self, request):
-        return HttpResponse(content='Hola mundo, soy Livingstone')
+        data = {
+            'name':'Livingsone Cano Bravo Data',
+            'years':28,
+            'codes':['Python','Django','React','Vue']
+        }
+
+        return render(request,'hello_world.html',context=data)
+
