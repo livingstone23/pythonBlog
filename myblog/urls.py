735c58e3bd4e5726bf6aad0c6908ccd9c3a4fbc3
livingstone23 <livingstone23@gmail.com>
2022-11-09 16:10:01 +0100

    Registrando una vista de Django, Video 33

--- myblog/urls.py
+++ myblog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts.views import HelloWorld
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('post/',HelloWorld.as_view())
 ]
